b6436922601b1329acf62dc22e6300925f06940b
Jeuwls <71440860+Jeuwls@users.noreply.github.com>
2022-12-26 22:18:07 +0100

    Trivial output test

--- Program.cs
+++ Program.cs
@@ -5,6 +5,14 @@ namespace REB {
         static void Main(string[] args) {
         // Display the number of command line arguments.
             Console.WriteLine(args.Length);
+            
+            DCRGraph gr = new DCRGraph();
+            gr.AddEvent("EventA");
+            gr.AddEvent("EventB");
+            gr.AddRelation("EventA", "EventB", RelationType.Condition);
+            gr.ExecuteActivity("EventB");
+            gr.ExecuteActivity("EventA");
+            gr.ExecuteActivity("EventB");
         }
     }
 }

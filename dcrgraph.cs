d55f97e17fb1080946131c92735f785e0d454f65
Jeuwls <71440860+Jeuwls@users.noreply.github.com>
2022-12-26 21:42:57 +0100

    Added AddEvent() (very basic)

--- dcrgraph.cs
+++ dcrgraph.cs
@@ -4,17 +4,34 @@ using System.Linq;
 namespace REB{
     class DCRGraph{
         List<Activity> Activities = new List<Activity>();
-        
 
         private List<Activity> included = new List<Activity>();
-        
+        private List<Activity> responding = new List<Activity>();
+        private List<Activity> executed = new List<Activity>();
+
         public List<Activity> GetIncluded(List<Activity> activities){
             return included;
         }
 
+        private void UpdateResponding()
+        {
+            responding = (List<Activity>)Activities.Where(x => x.IsPending());
+        }
+
         private void UpdateIncluded()
         {
-            included = (List<Activity>)Activities.Where(x => x.IsPending());
+            included = (List<Activity>)Activities.Where(x => x.Included);
+        }
+
+        private void UpdateExecuted()
+        {
+            executed = (List<Activity>)Activities.Where(x => x.Executed);
+        }
+
+        public void AddEvent(string nid)
+        {
+            Activity a = new Activity(nid);
+            Activities.Add(a);
         }
     }
 }

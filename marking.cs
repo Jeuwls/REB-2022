ef6f4a75660c4f32a7d1e59b3884beecca38e7be
Jeuwls <71440860+Jeuwls@users.noreply.github.com>
2022-12-26 21:27:45 +0100

    Marking object + ToString override

--- marking.cs
+++ marking.cs
@@ -1,6 +1,19 @@
+using System.Collections;
 namespace REB{
     public class Marking {
-        private List<Activity> included = new List<Activity>();
-
+        public bool Included { get; set; }
+        public bool Pending { get; set; }
+        public bool Executed { get; set; }
+        
+        public Marking(bool i, bool p, bool e)
+        {
+            Included = i;
+            Pending = p;
+            Executed = e;
+        }
+        public override string ToString()
+        {
+            return $"(Included: {Included}, Pending: {Pending}, Executed: {Executed})\n";
+        }
     }
 }
